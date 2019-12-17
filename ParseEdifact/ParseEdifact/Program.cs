e2b2ec8fa0807a78a34e033192435955d544069a
Marcus Beirne <marcus.beirne@gmail.com>
2019-12-17 22:19:00 +0000

    Parse EDI file into a string

--- ParseEdifact/ParseEdifact/Program.cs
+++ ParseEdifact/ParseEdifact/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace ParseEdifact
 {
@@ -7,6 +9,40 @@ namespace ParseEdifact
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Need to receive the message
+            // Read through the message to locate the 'LOC' statements
+            // Parse the 'LOC' statements
+            // Output the parsed data into arrays
+            string path = "D:\\Users\\marcus\\Projects\\Code\\Study\\ParseEdifact\\ParseEdifact\\ParseEdifact\\Data\\sample.edi";
+            var ediFileString = ParseEdiFileToString(path);
+            
+        }
+
+        /// <summary>
+        /// Read the input .edi file and output a string
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        private static string ParseEdiFileToString(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"File Not Found at the location: {path}");
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            using (FileStream fileStream = File.OpenRead(path))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fileStream.Read(b, 0, b.Length) > 0)
+                {
+                    stringBuilder.Append(temp.GetString(b));
+                }
+            }
+            return stringBuilder.ToString();
         }
     }
 }
